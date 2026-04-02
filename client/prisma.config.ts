b39129489d3ec680580f2a4453a92cb3a2956633
pedzmrx <pedromoraes750@gmail.com>
2026-04-02 13:54:05 -0400

    fix: removendo prisma.config que apontava para localhost

--- client/prisma.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@prisma/config';
-
-export default defineConfig({
-  datasource: {
-    url: "postgresql://admin:password123@localhost:5432/gym_db?schema=public",
-  },
-});
