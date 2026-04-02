2a244c5922948a92be550975538ca20eb05d9223
pedzmrx <pedromoraes750@gmail.com>
2026-04-02 17:03:16 -0400

    fix: remoçao do localhost para ajuste do prisma client

--- client/lib/prisma.ts
+++ client/lib/prisma.ts
@@ -1,13 +1,12 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 
-const connectionString = "postgresql://admin:password123@localhost:5432/gym_db?schema=public";
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool as any);
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter } as any);
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
